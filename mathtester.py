778d20d0cfdc50e66c72cc1475d9f381ae79b550
bennicia <bennicia@macstudent.com.au>
2020-06-01 12:27:14 +1000

    added the help bust still try ing to fix it and changed the data[1] to data[0] to fix a runtime area

--- mathtester.py
+++ mathtester.py
@@ -1,21 +1,28 @@
-level = input('What level would you like 1 (HARD multiplication), 2 (make(x)the subject), multiple choice questions 3 ')
+def checkanswer(answer):
+    userAns = input('your answer: ').upper()
+    if userAns == answer:
+        results.append(1)
+    else:
+        results.append(0)
+level = input('What level would you like 1 (HARD multiplication), 2 (make(x)the subject), multiple choice questions 3 of H (help) ')
 if level.strip() == '1':
     f = open('data 1')
 elif level.strip() == '2':
     f = open('data 2')
-else:
+elif level.strip() == '3':
     f = open('data 3')
+else :
+    f = open('data 4')
+
 results = []
 
 answers = []
+
 for line in f:
     data = (line.strip().split(","))
-    print(data[1])
+    print(data[0])
 
-    if input('your answer: ').upper() == data[0]:
-        results.append(1)
-    else:
-        results.append(0)
+    checkanswer(data[0])
     answers.append(data[0])
 
 print(results)
@@ -30,4 +37,3 @@ for i in range(len(results)):
     total = total + score
 
 print("your total score was", total, '/ 8')
-
